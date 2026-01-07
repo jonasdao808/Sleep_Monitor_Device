0f94645f845c6528254700cd89306b2fc5d67f4e
jonasdao808 <jonas.dao808@gmail.com>
2026-01-06 20:12:36 -0800

    Add comments to AlertManager.cpp

--- sleep_device/AlertManager.cpp
+++ sleep_device/AlertManager.cpp
@@ -2,17 +2,19 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-static const char* ssid = ""; // fill with wifi name
-static const char* password = ""; // fill with wifi password
+static const char* ssid = "CMDI"; // Wifi name
+static const char* password = "cmdi2020"; // Wifi password
 
 static String server = "http://maker.ifttt.com";
-static String eventName = ""; //fill with event name
-static String IFTTT_Key = ""; // fill with IFTTT key
+static String eventName = "sleep_monitor_trigger"; // event name for IFTTT
+static String IFTTT_Key = "dxqz3s1hMZV-FUHcWqTvKf"; // IFTTT key
 
 static unsigned long lastAlertTime = 0;
 static const unsigned long alertCooldown = 10000;
 
 void Alert_Init() {
+
+  // Initiate Wifi 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
@@ -23,22 +25,21 @@ void Alert_Init() {
   Serial.println("\nWiFi connected");
 }
 
-void Alert_Update() {
-  // placeholder for future logic (reconnect, etc.)
-}
-
+// Allow alert to be sent after cooldown
 bool Alert_CanSend() {
   return millis() - lastAlertTime > alertCooldown;
 }
 
 bool Alert_Send() {
-  String url = server + "/trigger/" + eventName + "/with/key/" + IFTTT_Key;
+  String url = server + "/trigger/" + eventName + "/with/key/" + IFTTT_Key; // IFTTT url
 
+  // Starts web connection
   HTTPClient http;
   http.begin(url);
   int code = http.GET();
   http.end();
 
+  // If failed connection
   if (code > 0) {
     lastAlertTime = millis();
     return true;
@@ -46,7 +47,7 @@ bool Alert_Send() {
   return false;
 }
 
+// Define time since last alert sent
 unsigned long Alert_TimeSinceLast() {
   return millis() - lastAlertTime;
 }
-
